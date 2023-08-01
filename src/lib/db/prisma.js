1b008ff7be7c174c3d4c2fbac9612fb22cd86b32
AirP0WeR <mgyp@mail.ru>
2023-08-01 12:47:34 +0300

    prisma db

---
+++ src/lib/db/prisma.js
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+
+const prismaBase = globalForPrisma.prisma ?? new PrismaClient();
+
+export const prisma = prismaBase.$extends({
+  query: {
+    cart: {
+      async update({ args, query }) {
+        args.data = { ...args.data, updatedAt: new Date() };
+        return query(args);
+      },
+    },
+  },
+});
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prismaBase;
